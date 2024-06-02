72095be8788266ad29f508db5b5c29123a6d628f
agupta17 <agupta17@opentext.com>
2024-06-02 15:13:51 +0530

    Spring AI app

---
+++ src/main/java/com/ai/example/ai_example/AIController.java
@@ -0,0 +1,38 @@
+package com.ai.example.ai_example;
+
+
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/ai")
+public class AIController {
+
+    @Autowired
+    private OllamaChatClient client;
+
+    private static final String PROMPT = "what is java language";
+
+    public AIController(OllamaChatClient client) {
+        this.client = client;
+    }
+
+    @GetMapping("/prompt")
+    public Flux<ChatResponse> promptResponse(
+            @RequestParam("prompt") String prompt
+    ) {
+        Prompt promptOb = new Prompt(prompt);
+        Flux<ChatResponse> response = client.stream(promptOb);
+        return response;
+
+    }
+
+}
+
